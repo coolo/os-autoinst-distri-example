225ee4696e58dd7bb77329b662e969095fad6d7d
Stephan Kulow <coolo@suse.de>
2021-01-23 09:20:21 +0100

    Install git

--- tests/boot.pm
+++ tests/boot.pm
@@ -8,6 +8,7 @@ use testapi;
 
 sub run {
     wait_serial("Welcome to openSUSE", 300);
+    assert_script_run('zypper -n install git-core');
     assert_script_run('git clone https://github.com/openSUSE/openSUSE-release-tools.git');
     assert_script_run('cd openSUSE-release-tools');
     assert_script_run('sed -i -e "s,../..:,$PWD:," dist/ci/docker-compose.yml');
