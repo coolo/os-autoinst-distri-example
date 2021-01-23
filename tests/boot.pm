cb29b8e8dffbd4b79e79c6a2a1f3ee66b675a675
Stephan Kulow <coolo@suse.de>
2021-01-23 11:06:17 +0100

    add user tester

--- tests/boot.pm
+++ tests/boot.pm
@@ -8,9 +8,10 @@ use testapi;
 
 sub run {
     wait_serial("Welcome to openSUSE", 300);
-    assert_script_run('zypper -n install git-core');
-    assert_script_run('git clone https://github.com/openSUSE/openSUSE-release-tools.git');
-    assert_script_run('cd openSUSE-release-tools');
+    assert_script_run('useradd -m tester');
+    assert_script_run('su - tester');
+    assert_script_run('git clone https://github.com/openSUSE/openSUSE-release-tools.git repo');
+    assert_script_run('cd repo');
     assert_script_run('sed -i -e "s,../..:,$PWD:," dist/ci/docker-compose.yml');
     assert_script_run('docker-compose -f dist/ci/docker-compose.yml run flaker');
     assert_script_run('docker-compose -f dist/ci/docker-compose.yaml run test');
