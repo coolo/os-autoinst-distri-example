76e7df7da1dc8cb9f5f506485921fc0fc59a0d04
Stephan Kulow <coolo@suse.de>
2021-01-23 09:14:38 +0100

    Fix syntax

--- tests/boot.pm
+++ tests/boot.pm
@@ -8,11 +8,11 @@ use testapi;
 
 sub run {
     wait_serial("Welcome to openSUSE", 300);
-    assert_script_run("git clone https://github.com/openSUSE/openSUSE-release-tools.git");
-    assert_script_run("cd openSUSE-release-tools");
-    assert_script_run("sed -i -e "s,../..:,$PWD:," dist/ci/docker-compose.yml");
-    assert_script_run("docker-compose -f dist/ci/docker-compose.yml run flaker");
-    assert_script_run("docker-compose -f dist/ci/docker-compose.yaml run test");
+    assert_script_run('git clone https://github.com/openSUSE/openSUSE-release-tools.git');
+    assert_script_run('cd openSUSE-release-tools');
+    assert_script_run('sed -i -e "s,../..:,$PWD:," dist/ci/docker-compose.yml');
+    assert_script_run('docker-compose -f dist/ci/docker-compose.yml run flaker');
+    assert_script_run('docker-compose -f dist/ci/docker-compose.yaml run test');
 }
 
 1;
